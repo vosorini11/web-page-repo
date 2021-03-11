ec7e3cd60192ad3383087cd26b69dae89bb582c2
vosorini11 <80409779+vosorini11@users.noreply.github.com>
2021-03-11 15:55:13 +0000

    Add files via upload
    New files on branch 1

---
+++ Loops05.java
@@ -0,0 +1,19 @@
+public class Loops05 {
+
+    public static void main(String[] args) {
+
+//    	int i = 100;
+
+    	System.out.println("A lone print outside the loop");
+
+//    	while (false) {
+//
+//    		System.out.println("i: " + i + " ");
+//
+//    		i += 100;
+//
+//    	}
+
+    }
+    
+}
